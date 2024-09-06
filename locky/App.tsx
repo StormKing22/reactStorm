14aa6d6795330e23469c07c07db2a1b990bec255
Guilhermedos <159535527+StormKing22@users.noreply.github.com>
2024-09-06 08:39:46 -0300

    teste3
    vamoo

--- locky/App.tsx
+++ locky/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react-native";
+import { StyleSheet, Text, View } from 'react-native' 
 
-export default function App() {
+export default function app() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>texto</Text>
       <StatusBar style="auto" />
     </View>
   );
